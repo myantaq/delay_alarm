55be9539b93e8512024904779f3a0222ef562677
myantaq <shimasuzu@live.jp>
2017-07-09 23:52:33 +0900

    bug fix

--- common.h
+++ common.h
@@ -7,7 +7,7 @@
 #define LIMIT(value, min, max) (((value)<(min))?(min):((value)>(max))?(max):(value))
 #define BIT_SET(reg, bit, value) ((reg)=((reg)&~(1<<(bit)))|((value)<<(bit)))
 
-#define BITS_MASK(sbit, nbit) (((2<<(nbit))-1)<<(sbit))
+#define BITS_MASK(sbit, nbit) (((1<<(nbit))-1)<<(sbit))
 #define BITS_SET(reg, sbit, nbit, value) ((reg)=((reg)&~BITS_MASK(sbit, nbit))|(((value)<<(sbit))&BITS_MASK(sbit, nbit)))
 
 #define BV(reg, bit) (((reg)>>(bit))&1)
